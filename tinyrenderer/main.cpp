09df29db3b366b8534965efcfbdfbb4bc7c2c3ce
skychx <skychx@hotmail.com>
2020-11-15 10:38:28 +0800

    Initial Commit

---
+++ tinyrenderer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  tinyrenderer
+//
+//  Created by skychx on 2020/11/15.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
