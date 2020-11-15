3e435f8cdff04351799d6ab305c6f2ae262413d5
skychx <skychx@hotmail.com>
2020-11-15 13:26:06 +0800

    Lesson1 加载 obj 模型

--- tinyrenderer/main.cpp
+++ tinyrenderer/main.cpp
@@ -5,9 +5,17 @@
 //  Created by skychx on 2020/11/15.
 //
 
+#include <vector>
+#include <cmath>
 #include "tgaimage.h"
+#include "model.h"
+#include "geometry.h"
+
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
+Model *model = NULL;
+const int width  = 800;
+const int height = 800;
 
 // 思路很简单，点连成线
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
@@ -54,11 +62,32 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 }
 
 int main(int argc, char** argv) {
-        TGAImage image(100, 100, TGAImage::RGB);
-        line(13, 20, 80, 40, image, white);
-        line(20, 13, 40, 80, image, red);
-        line(80, 40, 13, 20, image, red);
+//        TGAImage image(100, 100, TGAImage::RGB);
+//        line(13, 20, 80, 40, image, white);
+//        line(20, 13, 40, 80, image, red);
+//        line(80, 40, 13, 20, image, red);
+        if (2 == argc) {
+            model = new Model(argv[1]);
+        } else {
+            model = new Model("obj/african_head.obj");
+        }
+    
+        TGAImage image(width, height, TGAImage::RGB);
+        for (int i = 0; i < model->nfaces(); i++) {
+            std::vector<int> face = model->face(i);
+            for (int j = 0; j < 3; j++) {
+                Vec3f v0 = model->vert(face[j]);
+                Vec3f v1 = model->vert(face[(j + 1) % 3]);
+                int x0 = (v0.x + 1.) * width / 2.;
+                int y0 = (v0.y + 1.) * height / 2.;
+                int x1 = (v1.x + 1.) * width / 2.;
+                int y1 = (v1.y + 1.) * height / 2.;
+                line(x0, y0, x1, y1, image, white);
+            }
+        }
+
         image.flip_vertically();
         image.write_tga_file("output.tga");
+        delete model;
         return 0;
 }
---
+++ tinyrenderer/model.h
@@ -0,0 +1,27 @@
+//
+//  model.hpp
+//  tinyrenderer
+//
+//  Created by skychx on 2020/11/15.
+//
+
+#ifndef __MODEL_H__
+#define __MODEL_H__
+
+#include <vector>
+#include "geometry.h"
+
+class Model {
+private:
+    std::vector<Vec3f> verts_;
+    std::vector<std::vector<int> > faces_;
+public:
+    Model(const char *filename);
+    ~Model();
+    int nverts();
+    int nfaces();
+    Vec3f vert(int i);
+    std::vector<int> face(int idx);
+};
+
+#endif //__MODEL_H__
