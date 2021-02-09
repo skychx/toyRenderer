e7fd925c008e60c4b0b264bb2024fff17485a53a
skychx <skychx@hotmail.com>
2021-02-09 09:21:03 +0800

    ✨ day04-z-buffer 新分支

--- tinyrenderer/main.cpp
+++ tinyrenderer/main.cpp
@@ -23,87 +23,20 @@ Model *model = NULL;
 const int width  = 800;
 const int height = 800;
 
-// 思路很简单，点连成线
-void line(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color) {
-    // 处理比较陡的线：交换 x y 位置
-    bool steep = false;
-    if (std::abs(p0.x - p1.x) < std::abs(p0.y - p1.y)) {
-        std::swap(p0.x, p0.y);
-        std::swap(p1.x, p1.y);
-        steep = true;
-    }
-    // 处理 x0 > x1 的情况
-    if (p0.x > p1.x) {
-        std::swap(p0.x, p1.x);
-        std::swap(p0.y, p1.y);
-    }
-    // 缓存偏移量
-    int dx = p1.x - p0.x;
-    int dy = p1.y - p0.y;
-    // 存储误差
-    // float derror = std::abs(dy / float(dx)); // 每次循环增加的小误差
-    // float error = 0; // 累积误差
-    // 浮点运算肯定没有 int 快，我们想办法去掉浮点运算
-    int derror2 = std::abs(dy) * 2;
-    int error2 = 0;
-    int y = p0.y;
-    for (int x = p0.x; x <= p1.x; x++) {
-        if (steep) {
-            image.set(y, x, color);
-        } else {
-            image.set(x, y, color);
-        }
-        // 误差的处理：每次误差大于一个像素时，y 就要进位 1，相应的累积误差也要减小 1
-        // error += derror;
-        // if (error > .5) {
-            // y += (p1.y > p0.y ? 1 : -1);
-            // error -= 1.;
-        // }
-        error2 += derror2;
-        if (error2 > dx) {
-            y += (p1.y > p0.y ? 1 : -1);
-            error2 -= dx * 2;
-        }
-    }
-}
-
-// 利用叉乘判断是否在三角形内部
-bool isInside(Vec3f *pts, Vec2i P) {
-    Vec2i AB(pts[1].x - pts[0].x, pts[1].y - pts[0].y);
-    Vec2i BC(pts[2].x - pts[1].x, pts[2].y - pts[1].y);
-    Vec2i CA(pts[0].x - pts[2].x, pts[0].y - pts[2].y);
-
-    Vec2i AP(P.x - pts[0].x, P.y - pts[0].y);
-    Vec2i BP(P.x - pts[1].x, P.y - pts[1].y);
-    Vec2i CP(P.x - pts[2].x, P.y - pts[2].y);
-
-    // 叉乘计算
-    if(
-       (AB.x * AP.y - AP.x * AB.y) >= 0 &&
-       (BC.x * BP.y - BP.x * BC.y) >= 0 &&
-       (CA.x * CP.y - CP.x * CA.y) >= 0
-    ) {
-        return true;
-    }
-    return false;
-}
-
-// 利用重心坐标求解，返回三角形的重心坐标
-Vec3f barycentric(Vec3f A, Vec3f B, Vec3f C, Vec3f P) {
+// 利用重心坐标判断点是否在三角形内部
+Vec3f barycentric(Vec3f *pts, Vec3f P) {
+    Vec3i x(pts[1].x - pts[0].x, pts[2].x - pts[0].x, pts[0].x - P.x);
+    Vec3i y(pts[1].y - pts[0].y, pts[2].y - pts[0].y, pts[0].y - P.y);
     
-    Vec3f x(C[0] - A[0], B[0] - A[0], A[0] - P[0]); // AB AC PA 在 x 上的分量
-    Vec3f y(C[1] - A[1], B[1] - A[1], A[1] - P[1]); // AB AC PA 在 y 上的分量
-
-    Vec3f u = cross(x, y); // u 向量和 x y 点乘都为 0，所以 u 垂直于 xy 平面
+    // u 向量和 x y 向量的点积为 0，所以 x y 向量叉乘可以得到 u 向量
+    Vec3i u = cross(x, y);
     
-    // dont forget that u[2] is integer. If it is zero then triangle ABC is degenerate
-    // 根据 u[2] 判断法线是向内的还是向外的，向内的抛弃，向外的保留并归一化
-    if (std::abs(u[2]) > 1e-2) {
-        return Vec3f(1.f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
+    // 由于 A, B, C, P 的坐标都是 int 类型，所以 u.z 必定是 int 类型，取值范围为 ..., -2, -1, 0, 1, 2, ...
+    // 所以 u.z 绝对值小于 1 意味着三角形退化了，需要舍弃
+    if(std::abs(u.z) < 1) {
+        return Vec3f(-1, 1, 1);
     }
-    
-    // in this case generate negative coordinates, it will be thrown away by the rasterizator
-    return Vec3f(-1, 1, 1);
+    return Vec3f(1.f- (u.x+u.y) / (float)u.z, u.x / (float)u.z, u.y / (float)u.z);
 }
 
 // 自己实现的三角形光栅化函数
@@ -115,9 +48,9 @@ void triangle(Vec3f *pts, float *zbuffer, TGAImage &image, TGAColor color) {
     Vec2f clamp(image.get_width() - 1, image.get_height() - 1); // 图片的边界
     // 查找包围盒边界
     for (int i = 0; i < 3; i++) {
-        // 第一层循环，遍历 pts
+        // 第一层循环，遍历三角形的三个顶点
         for (int j = 0; j < 2; j++) {
-            // 第二层循环，遍历 Vec2i
+            // 第二层循环，根据顶点数值缩小包围盒的范围
             boxmin.x = std::max(0.f,        std::min(boxmin.x, pts[i].x));
             boxmin.y = std::max(0.f,        std::min(boxmin.y, pts[i].y));
             
@@ -128,24 +61,21 @@ void triangle(Vec3f *pts, float *zbuffer, TGAImage &image, TGAColor color) {
     
 //    std::cout << "boxmin: " << boxmin << "boxmax: "  << boxmax << std::endl;
     
-    // 步骤二：对包围盒里的每一个像素进行遍历
+    // 步骤二：包围盒内的每一个像素和三角形顶点连线做叉乘
     Vec3f P;
     for (P.x = boxmin.x; P.x <= boxmax.x; P.x++) {
         for (P.y = boxmin.y; P.y <= boxmax.y; P.y++) {
-            // if (isInside(pts, P)) {
-            //     image.set(P.x, P.y, color);
-            // }
-            Vec3f bc = barycentric(pts[0], pts[1], pts[2], P); // bc 是 Barycentric Coordinates 的缩写
+            Vec3f bc_screen = barycentric(pts, P); // bc 是 Barycentric Coordinates 的缩写
 
-            // 重心坐标某一项小于 0，说明在三角形外，跳过不绘制
-            if (bc.x < 0 || bc.y < 0 || bc.z < 0) {
+            // bc_screen 某个分量小于 0 则表示此点在三角形外（认为边也是三角形的一部分）
+            if (bc_screen.x < 0 || bc_screen.y < 0 || bc_screen.z < 0) {
                 continue;
             }
             
             // 计算当前像素的 zbuffer
             P.z = 0;
             for (int i = 0; i < 3; i++) {
-                P.z += pts[i][2] * bc[i];
+                P.z += pts[i][2] * bc_screen[i];
             }
             
             // 更新总的 zbuffer 并绘制
@@ -186,10 +116,10 @@ void drawModelTriangle() {
     //  [4, 5, 6],   =>   4, 5, 6,
     //  [7, 8, 9]]        7, 8, 9],
     float *zbuffer = new float[width * height];
-    for (int i = width * height;
-         i--;
-         zbuffer[i] = -std::numeric_limits<float>::max()
-    );
+
+    for (int i=0; i < width * height; i++) {
+        zbuffer[i] = -std::numeric_limits<float>::max();
+    }
     
     // 遍历所有三角形
     for (int i = 0; i < model->nfaces(); i++) {
@@ -214,7 +144,7 @@ void drawModelTriangle() {
     }
     
     frame.flip_vertically();
-    frame.write_tga_file("output/lesson03_z_buffer.tga");
+    frame.write_tga_file("output/day04_z_buffer.tga");
     
     delete model;
 }
